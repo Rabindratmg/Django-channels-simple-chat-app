74c7268fe18c9b9610c296b515b73d74eb69946c
Rabindra <rabee.tmg123@gmail.com>
2021-06-26 12:37:32 +0545

    Docker images

--- DjangoChannels/asgi.py
+++ DjangoChannels/asgi.py
@@ -7,17 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChannels.settings')
+
+# application = get_asgi_application()
+
+
+
+
 # mysite/asgi.py
+
 import os
 
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChannels.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     # Just HTTP for now. (We can add other protocols later.)
 })
-
-
